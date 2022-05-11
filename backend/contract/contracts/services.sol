92e28d7fd1ee9bb587eebabe74cb00b5a5232b3e
Akarsh Puri <nanda5687@gmail.com>
2022-05-11 11:45:47 +0530

    Added Contract

--- backend/contract/contracts/services.sol
+++ backend/contract/contracts/services.sol
@@ -4,22 +4,27 @@ pragma experimental ABIEncoderV2;
 
 
 contract Services {
-    struct service { 
+    struct log { 
         uint256 startTime;
         uint256 endTime;
+        string[] memberIDs;
         string userID;
+        uint256 serviceID;
     }
-    mapping(string => service) serviceMap;
+    mapping(string => log[]) serviceMap;
 
-    function addService(uint256 _startTime, uint256 _endTime,string memory _userID) public {
-        service memory s;
+    function addService(uint256 _startTime, uint256 _endTime,string[] memory _memberIDs, string memory _userID, uint256 _serviceID) public {
+        log memory s;
         s.startTime = _startTime;
         s.endTime = _endTime;
         s.userID = _userID;
-        serviceMap[_userID] = s;
+        s.memberIDs = _memberIDs;
+        s.serviceID = _serviceID;
+        serviceMap[_userID].push(s);
     }
 
-    function getService(string memory _userID) public view returns(service memory){
+    function getService(string memory _userID) public view returns(log[] memory){
         return serviceMap[_userID];
     }
+
 }
